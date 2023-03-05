1c3abb31526f503e67932dcc49862cffca78407d
Anna Nalimova <avnalimova@mail.ru>
2023-03-05 11:51:27 +0300

    41,43

---
+++ 41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры М чисел. 
+// Посчитайте сколько чисел больше 0 ввел пользователь.
+Console.WriteLine("Введите числа массива через пробел: ");
+string elements=Console.ReadLine();
+int[]firstarray=GetArrayFromString(elements);
+int res = GetNumber(firstarray);
+Console.WriteLine(res);
+int  GetNumber(int[] massive)
+{
+    int count=0;
+    for(int i=0;i<massive.Length;i++)
+    {
+       if(massive[i]>0)  
+       count++;
+    }
+    return  count;
+}
+int[] GetArrayFromString(string stringArray)
+{
+
+  string[]nums=stringArray.Split(" ");
+  int[]result = new int[nums.Length];
+  for( int i=0;i<nums.Length;i++){
+
+      result [i]=int.Parse(nums[i]);
+  }
+  return result;
+}
+
+ 
---
+++ 43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу,которая найдет точку пересечения двух прямых,
+// заданных уравнениями y=k1*x+b1,
+//                      y=k2*x+b2
+// значения b1,k1,b2,k2 вводит пользователь.
+double[]  DataInput() { 
+  double[] massive =new double[4];
+ Console.WriteLine("Введите значение  b1 : ");
+ massive[0]= double.Parse(Console.ReadLine());
+ Console.WriteLine("Введите значение k1 : ");
+ massive[1]= double.Parse(Console.ReadLine());
+ Console.WriteLine("Введите значение b2 : ");
+ massive[2]= double.Parse(Console.ReadLine());
+ Console.WriteLine("Введите значение k2 : ");
+ massive[3]= double.Parse(Console.ReadLine());
+ return massive;
+}
+
+double[] FindPoint(double[] mass)
+{
+  double [] array=new double[2];
+  array[0]=(mass[1]-mass[3])/(mass[2]-mass[0]);
+  array[1]=mass[1]*array[0]+mass[0];
+return array;  
+}   
+Console.WriteLine($"({String.Join("; ", FindPoint(DataInput()))}) ");
